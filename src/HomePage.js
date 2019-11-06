3be40587e2161d0a1f80d58e9366bfa48811b42c
AllanRaf <rafale.uk@googlemail.com>
2019-11-06 22:44:53 +0100

    first commit add home page

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+//instaimage-client/components/HomePage.js
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <button>Register as a new user</button>
+                <button>Log in if you have already registered</button>
+            </div>
+        )
+    }
+}
